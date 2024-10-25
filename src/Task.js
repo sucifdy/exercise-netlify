9fd067f8717ffbb1dce9d5cb6037201c9ca64310
SUCI <suciffdyy@gmail.com>
2024-10-25 16:45:38 +0700

    Update student info

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://playful-kataifi-a0e852.netlify.app/";
+const studentName = "Suci Fitriyah Darmayanti";
+const studentId = "FS12194279";
+
+export { url, studentName, studentId };
